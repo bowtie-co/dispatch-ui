f0eafd834a7bcdcce6339a31fc4d10c28373d085
nick arrasate <Narrasate22@icloud.com>
2026-06-05 13:53:57 -0700

    adjust some theme labels for calrity. make sure email and page are sharing the button text field

--- src/organisms/Theme/Form.jsx
+++ src/organisms/Theme/Form.jsx
@@ -554,6 +554,12 @@ export const ThemeForm = (props) => {
                 </Col>
               </FormGroup>
               <FormGroup row>
+                <Label md={4} lg={3} for='pageDisclaimerText'>Page Share Text (disclaimer)</Label>
+                <Col md={8}>
+                  <Input key='pageDisclaimerText' type='textarea' name='pageDisclaimerText' id='pageDisclaimerText' onChange={(e) => setPageDisclaimerText(e.target.value)} defaultValue={pageDisclaimerText}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
                 <Label md={4} lg={3} for='shareTitleText'>Share Title Text</Label>
                 <Col md={8}>
                   <Input key='shareTitleText' type='text' name='shareTitleText' id='shareTitleText' onChange={(e) => setShareTitleText(e.target.value)} defaultValue={shareTitleText} placeholder='Dispatch Video / Dispatch Image' />
@@ -563,12 +569,6 @@ export const ThemeForm = (props) => {
                 <Label md={4} lg={3} for='shareTagText'>Share Tag Text</Label>
                 <Col md={8}>
                   <Input key='shareTagText' type='text' name='shareTagText' id='shareTagText' onChange={(e) => setShareTagText(e.target.value)} defaultValue={shareTagText} placeholder='Share your keepsake and tag us!' />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label md={4} lg={3} for='pageDisclaimerText'>Share Text</Label>
-                <Col md={8}>
-                  <Input key='pageDisclaimerText' type='textarea' name='pageDisclaimerText' id='pageDisclaimerText' onChange={(e) => setPageDisclaimerText(e.target.value)} defaultValue={pageDisclaimerText}/>
                 </Col>
               </FormGroup>
               <FormGroup row>
